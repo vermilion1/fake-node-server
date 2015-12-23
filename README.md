64998d04f67b216818081d8ea299505276e87ac5
Vitalii Petrychuk <vpetrychuk@atlassian.com>
2015-12-23 12:35:30 +0100

    Add few more echo endpoints, update readme

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Fake Server
 
 
 The server with a set of endpoints that could be used for test purposes.  
+Cross Origin Resource Sharing (CORS) is enabled by default.  
 Feel free to add endpoints for your needs.
 
 
@@ -22,9 +23,35 @@ Endpoints
 - [/delay/:amount](http://localhost:3030/delay/1000)  
   Returns empty JSON object right after passed `amount` of milliseconds. 
 
-- [/echo/:json](http://localhost:3030/echo/%7B%22prop%22:%22value%22%7D)  
-  Returns passed JSON. Example:  
+- [/echo](http://localhost:3030/echo)  
+  Returns empty response.
+
+- [/echo/json](http://localhost:3030/echo/json)  
+  Returns empty JSON response.
+  
+- [/echo/json/:value](http://localhost:3030/echo/json/%7B%22prop%22:%22value%22%7D)  
+  Returns passed JSON `value`. Example:  
     
         fetch('http://localhost:3030/echo/' + encodeURIComponent(JSON.stringify({prop: 'value'})))
           .then(function (response) { return response.json(); })
           .then(function (data) { console.log(data); }) // prints {prop: 'value'}
+
+- [/echo/xml](http://localhost:3030/echo/xml)
+  Returns empty XML response.
+
+- [/echo/xml/:value](http://localhost:3030/echo/xml/%3Chey%20%2F%3E)
+  Returns passed XML `value`. Example:  
+    
+        fetch('http://localhost:3030/echo/xml/' + encodeURIComponent('<hey />'))
+          .then(function (response) { return response.text(); })
+          .then(function (data) { console.log(data); }) // prints <hey />
+
+- [/echo/html](http://localhost:3030/echo/html)
+  Returns empty HTML response.
+
+- [/echo/html/:value](http://localhost:3030/echo/html/%3Cbutton%3Ehey%3C%2Fbutton%3E)
+  Returns passed HTML `value`. Example:  
+    
+        fetch('http://localhost:3030/echo/html/' + encodeURIComponent('<button>hey</button>'))
+          .then(function (response) { return response.text(); })
+          .then(function (data) { console.log(data); }) // prints <button>hey</button>
--- index.js
+++ index.js
@@ -7,6 +7,33 @@ var app = express();
 var PORT = 3030;
 var NOOP = function () {};
 
+var EMPTY_RESPONSE = function (req, res) {
+  res.end();
+};
+
+var JSON_RESPONSE = function (value) {
+  return function (req, res) {
+    try {
+      res.json(typeof value === 'string' ? JSON.parse(value) : value);
+    }
+    catch (e) {
+      res.json(createError('Invalid JSON input'));
+    }
+  };
+};
+
+var XML_RESPONSE = function (value) {
+  return function (req, res) {
+    res.set('Content-Type', 'text/xml').end(value);
+  };
+};
+
+var HTML_RESPONSE = function (value) {
+  return function (req, res) {
+    res.set('Content-Type', 'text/html').end(value);
+  };
+};
+
 /* ---------------------------------------------------------------------------------------------------------- Helpers */
 
 function createError(message) {
@@ -20,17 +47,25 @@ app.use(cors());
 /* -------------------------------------------------------------------------------------------------------- Endpoints */
 
 app.get('/delay', NOOP);
-app.get('/delay/:amount', function (req, res, next) {
-  setTimeout(res.json.bind(res, {}), req.params.amount);
+app.get('/delay/:amount', function (req, res) {
+  setTimeout(JSON_RESPONSE({}).bind(null, req, res), req.params.amount);
 });
 
-app.get('/echo/:value', function (req, res, next) {
-  try {
-    res.json(JSON.parse(req.params.value));
-  }
-  catch (e) {
-    res.json(createError('Invalid JSON input'));
-  }
+app.get('/echo', EMPTY_RESPONSE);
+
+app.get('/echo/xml', XML_RESPONSE());
+app.get('/echo/xml/:value', function (req, res) {
+  XML_RESPONSE(req.params.value).apply(this, arguments);
+});
+
+app.get('/echo/html', HTML_RESPONSE());
+app.get('/echo/html/:value', function (req, res) {
+  HTML_RESPONSE(req.params.value).apply(this, arguments);
+});
+
+app.get('/echo/json', JSON_RESPONSE({}));
+app.get('/echo/json/:value', function (req, res) {
+  JSON_RESPONSE(req.params.value).apply(this, arguments);
 });
 
 /* ----------------------------------------------------------------------------------------------------------- Server */
